03786702652df9780f1d01f2689cffe3a4b19bfc
Peter Dekkers <peter@jbaron.com>
2023-01-06 11:01:28 +0100

    Added more integration examples

--- samples/integration.kt
+++ samples/integration.kt
@@ -4,15 +4,16 @@ import org.roboquant.Roboquant
 import org.roboquant.alpaca.AlpacaBroker
 import org.roboquant.alpaca.AlpacaHistoricFeed
 import org.roboquant.alpaca.AlpacaLiveFeed
-import org.roboquant.common.Asset
-import org.roboquant.common.Timeframe
-import org.roboquant.common.days
-import org.roboquant.common.minutes
+import org.roboquant.brokers.ECBExchangeRates
+import org.roboquant.common.*
 import org.roboquant.oanda.OANDABroker
 import org.roboquant.oanda.OANDAHistoricFeed
 import org.roboquant.oanda.OANDALiveFeed
 import org.roboquant.orders.MarketOrder
+import org.roboquant.polygon.PolygonHistoricFeed
+import org.roboquant.polygon.PolygonLiveFeed
 import org.roboquant.yahoo.YahooHistoricFeed
+import java.time.Instant
 
 
 fun yahooFeed() {
@@ -42,6 +43,45 @@ fun oandaLiveFeed(roboquant: Roboquant) {
     roboquant.run(feed,tf)
     // end::oandalive[]
 }
+
+
+
+fun polygonHistoricFeed() {
+    // tag::polygonhistoric[]
+    val feed = PolygonHistoricFeed()
+    val tf = Timeframe.past(100.days)
+    feed.retrieve("AAPL", "JPM", "TSLA", timeframe = tf)
+    // end::polygonhistoric[]
+}
+
+
+fun ecb() {
+    // tag::ecb[]
+    // Download the latest rates from the ECB website
+    // The results are cached by default
+    Config.exchangeRates = ECBExchangeRates.fromWeb()
+
+    // Create a wallet with different currencies
+    val wallet = 100.EUR + 20.USD + 1000.JPY
+
+    // What is the conversion today in GBP
+    wallet.convert(Currency.GBP)
+
+    // What would the same conversion been 5 years ago
+    wallet.convert(Currency.GBP, Instant.now() - 5.years)
+    // end::ecb[]
+}
+
+suspend fun polygonLiveFeed(roboquant: Roboquant) {
+    // tag::polygonlive[]
+    val feed = PolygonLiveFeed()
+    feed.subscribe("AAPL", "JPM", "TSLA")
+
+    val tf = Timeframe.next(120.minutes)
+    roboquant.run(feed,tf)
+    // end::polygonlive[]
+}
+
 
 
 fun oandaBroker() {
